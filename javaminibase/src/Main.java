e75fe1415fd73a3095a55a24c9523aa751c7f5aa
esaifude <esaifude@asu.edu>
2018-02-21 17:18:59 -0700

    Added a common main method to run tests

---
+++ javaminibase/src/Main.java
@@ -0,0 +1,16 @@
+import tests.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //BMTest.runTests();
+        //DBTest.runTests();
+        //HFTest.runTests();
+        //BTTest.runTests();
+        IndexTest.runTests();
+        //JoinTest.runTests();
+        //SortTest.runTests();
+        //SM_JoinTest.runTests();
+    }
+}
