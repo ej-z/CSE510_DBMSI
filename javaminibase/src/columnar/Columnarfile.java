d18ad4152e56e5374d891ec1b8c4369b1334c889
Archanaseshakrishnan <archanaseshakrishnan94@gmail.com>
2018-02-26 23:19:04 -0700

    Add files via upload

--- javaminibase/src/columnar/Columnarfile.java
+++ javaminibase/src/columnar/Columnarfile.java
@@ -5,18 +5,26 @@ import java.io.IOException;
 import global.AttrType;
 import global.Convert;
 import global.RID;
+import heap.FieldNumberOutOfBoundException;
+import heap.FileAlreadyDeletedException;
+import heap.HFBufMgrException;
+import heap.HFDiskMgrException;
 import heap.Heapfile;
+import heap.InvalidSlotNumberException;
+import heap.InvalidTupleSizeException;
+import heap.InvalidTypeException;
 import heap.Tuple;
 
 public class Columnarfile_trial {
-	private static int numColumns;
-	private AttrType[] type;
-	Heapfile[] hf;
-	boolean status = true;    
-    private String fname;
+	private static short numColumns;
+	private AttrType[] atype = null;
+	short[] attrsizes;
+	Heapfile[] hf = null;  
+    private String fname = null;
     /*Convention: hf[] => 0 for hdr file; the rest for tables. The implementation is modified from HFTest.java*/
 	Columnarfile_trial(java.lang.String name, int numcols, AttrType[] types) throws IOException{
 		RID rid1 = new RID();
+		boolean status = true;  
 		try{
 			hf = new Heapfile[numcols+1];
 			//hf[0] for header file by default
@@ -29,12 +37,33 @@ public class Columnarfile_trial {
 		}
 		if(status==true){
 			//initializing member variables
-			numColumns = numcols;	
+			numColumns = (short)(numcols);	
 			this.fname = name;
-			for(int i=0;i<numColumns;i++){
-				type[i] = new AttrType(types[i].attrType);
+			//for TID
+			atype = new AttrType[numColumns];
+			attrsizes=new short[numColumns];
+			
+			for(int i=0;i<numcols;i++){
+				atype[i] = new AttrType(types[i].attrType);
+				switch(types[i].attrType){
+					case 0:
+						//assuming only numbers
+						break;
+					case 1:
+						attrsizes[i]=4;
+						break;
+					case 2:
+						attrsizes[i]=4;
+						break;
+					case 3:
+						attrsizes[i]=1;
+						break;
+					case 4:
+						attrsizes[i]=0;
+						break;
+				}
 			}
-			System.out.println("Inserting "+numcols+" records");
+			System.out.println("Inserting "+numColumns+" records");
 			//.hdr file initialization hf[0]
 			for(int i=0;i<numColumns;i++){				
 	            //data = "name.colnumber" + type[i].attrType
@@ -42,6 +71,8 @@ public class Columnarfile_trial {
 	            sb.append(this.fname);
 	            sb.append(".");
 	            sb.append(String.valueOf(i));
+	            sb.append("--");
+	            sb.append(String.valueOf(atype[i].attrType));
 	            String datastr = sb.toString();
 	            
 	            DummyRecord rec = new DummyRecord(datastr.getBytes());
@@ -54,9 +85,9 @@ public class Columnarfile_trial {
                     e.printStackTrace();
                 }
 			}
-			//allocating memory for the
+			//allocating memory for the others
 			try{
-				for(int i=0;i<numColumns;i++){
+				for(int i=1;i<numColumns;i++){
 					hf[i] = new Heapfile(name+String.valueOf(i));
 				}
 			}
@@ -65,6 +96,69 @@ public class Columnarfile_trial {
 				e.printStackTrace();
 			}
 		}
+	}
+	public void deleteColumnarFile() throws InvalidSlotNumberException, FileAlreadyDeletedException, InvalidTupleSizeException, HFBufMgrException, HFDiskMgrException, IOException{
+		for(int i=0;i<=numColumns;i++){
+			hf[i].deleteFile();
+		}
+		hf = null;
+		atype = null;
+		fname = null;
+		numColumns = 0;
+	}
+	public TID insertTuple(byte[] tuplePtr) throws IOException, InvalidTypeException, InvalidTupleSizeException, FieldNumberOutOfBoundException{
+		Tuple temp = new Tuple();
+		boolean status = true;
+		temp.setHdr(numColumns, atype, attrsizes);
+		temp.tupleInit(tuplePtr, 0, tuplePtr.length);
+		for(int i=0;i<numColumns;i++){
+			switch(atype[i].attrType){
+				case 0:
+					//string
+					break;
+				case 1:
+					//integer
+					int datafromtuple = temp.getIntFld(i);
+					DummyRecord rec = new DummyRecord(String.valueOf(datafromtuple).getBytes());
+		            try {
+		            	RID rid1=new RID();
+		            	byte[] one = String.valueOf(i).getBytes();
+		            	byte[] two = rec.toByteArray();
+		            	byte[] three = new byte[one.length+two.length];
+		            	System.arraycopy(one, 0, three, 0, one.length);
+		            	System.arraycopy(two, 0, three, one.length, two.length);
+		                rid1 = hf[i+1].insertRecord(three);
+		            }
+		            catch (Exception e) {
+	                    status = false;
+	                    System.err.println("*** Error inserting record " + (i) + "\n");
+	                    e.printStackTrace();
+	                }
+		            break;
+				case 2:
+					//real
+					float datafromtuple1 = temp.getFloFld(i);
+					DummyRecord rec1 = new DummyRecord(String.valueOf(datafromtuple1).getBytes());
+		            try {
+		            	RID rid1=new RID();
+		            	byte[] one = String.valueOf(i).getBytes();
+		            	byte[] two = rec1.toByteArray();
+		            	byte[] three = new byte[one.length+two.length];
+		            	System.arraycopy(one, 0, three, 0, one.length);
+		            	System.arraycopy(two, 0, three, one.length, two.length);
+		                rid1 = hf[i+1].insertRecord(three);
+		            }
+		            catch (Exception e) {
+	                    status = false;
+	                    System.err.println("*** Error inserting record " + (i) + "\n");
+	                    e.printStackTrace();
+	                }
+		            break;
+		        default:
+		        	//do nothing
+			}
+		}
+		return null;	
 	}
 }
 
