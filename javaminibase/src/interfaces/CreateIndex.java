2ac8a9fa19cb8e147b38fdc9aaa872ea993d854a
ej-z <ejaz.s.1991@gmail.com>
2018-04-17 20:12:54 -0700

    Modified create index interface

--- javaminibase/src/interfaces/CreateIndex.java
+++ javaminibase/src/interfaces/CreateIndex.java
@@ -2,138 +2,45 @@ package interfaces;
 
 import columnar.Columnarfile;
 import diskmgr.PCounter;
-import global.AttrType;
 import global.SystemDefs;
-import heap.HFBufMgrException;
-import heap.HFDiskMgrException;
-import heap.HFException;
-import tests.TestDriver;
-
-import java.io.IOException;
 
 import static global.GlobalConst.NUMBUF;
 
-class IndexTestDriver extends TestDriver {
-
-    private  int numPages = 1000;
-    private String dbName;
-    private String colFilename;
-    private String colName;
-    private String indexType;
-    private int numCols;
-    AttrType[] types;
-    short[] sizes;
-
-    //private boolean delete = true;
-    public IndexTestDriver() {
-        super("BatchInsert");
-    }
-
-    public IndexTestDriver(String columnName, String columnDBName, String columnarFileName, String indexTypeVal) {
-        super(columnDBName);
-        colName = columnName;
-        dbName = columnDBName;
-        colFilename = columnarFileName;
-        indexType = indexTypeVal;
-    }
-
-    public boolean runTests() {
-
-        System.out.println("\n" + "Running " + testName() + " tests...." + dbpath+"\n");
-
-        SystemDefs sysdef = new SystemDefs(dbpath, 0, NUMBUF, "Clock");
-
-        // Kill anything that might be hanging around
-        String newdbpath;
-        String newlogpath;
-        String remove_logcmd;
-        String remove_dbcmd;
-        String remove_cmd = isUnix()? "/bin/rm -rf " : "cmd /c del /f ";
-
-        newdbpath = dbpath;
-        newlogpath = logpath;
-
-        remove_logcmd = remove_cmd + logpath;
-        remove_dbcmd = remove_cmd + dbpath;
-
-        boolean _pass = runAllTests();
-        try {
-            SystemDefs.JavabaseBM.flushAllPages();
-            SystemDefs.JavabaseDB.closeDB();
-        }catch (Exception e) {
-            System.out.println("coming from here");
-            System.err.println("error: " + e);
-        }
-
-
-        System.out.println("Reads: "+PCounter.rcounter);
-        System.out.println("Writes: "+PCounter.wcounter);
-        System.out.print("\n" + "..." + testName() + " tests ");
-        System.out.print(_pass == OK ? "completely successfully" : "failed");
-        System.out.print(".\n\n");
-
-        return _pass;
-
-    }
-
-    protected boolean test1(){
-
-        try {
-            Columnarfile cf = new Columnarfile(colFilename);
-            int colno = cf.getAttributePosition(colName);
-
-            if (indexType.equals("BITMAP")) {
-                //cf.createBitMapIndex(val, value);
-                cf.createAllBitMapIndexForColumn(colno);
-            }
-            else {
-                cf.createBTreeIndex((short)colno);
-            }
-        } catch (HFException | HFBufMgrException | HFDiskMgrException | IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
-    protected String testName() {
-
-        return "Index ";
-    }
-}
-
 public class CreateIndex {
 
-    public static String columnDBName;
-    public static String columnarFileName;
-    public static String columnName;
-    public static String indexType;
+    public static void main(String[] args) throws Exception {
+        // Query Skeleton: COLUMNDB COLUMNARFILE COLUMNNAME INDEXTYPE
+        // Example Query: testColumnDB columnarTable columnName BITMAP|BTREE
+        // Last attribute specifies whether to create a new Db or open an existing one
+        String columnDB = args[0];
+        String columnarFile = args[1];
+        String columnName = args[2];
+        String indexType = args[3];
 
-    public static void runTests() {
+        String dbpath = InterfaceUtils.dbPath(columnDB);
+        SystemDefs sysdef = new SystemDefs(dbpath, 0, NUMBUF, "Clock");
 
-        IndexTestDriver cd = new IndexTestDriver(columnName, columnDBName, columnarFileName, indexType);
-        cd.runTests();
+        runInterface(columnarFile, columnName, indexType);
+
+        SystemDefs.JavabaseBM.flushAllPages();
+        SystemDefs.JavabaseDB.closeDB();
+
+        System.out.println("Reads: " + PCounter.rcounter);
+        System.out.println("Writes: " + PCounter.wcounter);
     }
 
-    public static void main(String[] argvs) {
+    private static void runInterface(String columnarFile, String columnName, String indexType) throws Exception {
 
-        try {
-            CreateIndex indexTest = new CreateIndex();
+        Columnarfile cf = new Columnarfile(columnarFile);
+        int colno = cf.getAttributePosition(columnName);
 
-            columnName = argvs[2];
-            columnDBName = argvs[0];
-            columnarFileName = argvs[1];
-            indexType = argvs[3];
-            indexTest.runTests();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Error encountered during buffer manager tests:\n");
-            Runtime.getRuntime().exit(1);
+        if (indexType.equals("BITMAP")) {
+            cf.createAllBitMapIndexForColumn(colno);
+        } else {
+            cf.createBTreeIndex(colno);
         }
+        cf.close();
+
+        System.out.println(indexType + " created successfully on "+columnarFile+":"+columnName);
     }
 }
